1ad2389b2beeb70670d904e1605d86a18e3a9ebb
Marek Hrušovský <xhruso00@gmail.com>
2018-11-12 20:07:46 +0100

    Added more

--- MyMapView.m
+++ MyMapView.m
@@ -1,4 +1,5 @@
 #import "MyMapView.h"
+#import <CoreLocation/CoreLocation.h>
 
 @implementation MyMapView
 
@@ -6,5 +7,25 @@
 {
     return YES;
 }
+
+- (NSMenu *)mapView:(MKMapView *)mapView menuForEvent:(NSEvent *)event
+{
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"Map View Menu"];
+    CLLocationCoordinate2D coord = [mapView convertPoint:[event locationInWindow] toCoordinateFromView:nil];
+//    if (CLLocationCoordinate2DIsValid(coord)) {
+//        CLLocation *location = [[CLLocation alloc] initWithLatitude:coord.latitude longitude:coord.longitude];
+//        NSMenuItem *item = [[NSMenuItem alloc] initWithTitle:@"Drop Pin" action:@selector(dropPin:) keyEquivalent:@""];
+//        [item setRepresentedObject:location];
+//        [menu addItem:item];
+//    }
+    return menu;
+}
+
+//- (nullable NSMenu *)menuForEvent:(NSEvent *)event
+//{
+//    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"Map View Menu"];
+//
+//    return menu;
+//}
 
 @end
